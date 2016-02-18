09e58bdf8f1b18b3393b65ecbaf0c1fb5f7c1885
Marcin Kik <mki1967@gmail.com>
2016-02-18 15:49:51 +0100

    przykład stereoskopowy

---
+++ test-canvas2d/canvas2d_test2.js
@@ -0,0 +1,188 @@
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+
+var canvas2 = document.getElementById("myCanvas2");
+var ctx2 = canvas2.getContext("2d");
+
+var sin=Math.sin;
+var cos=Math.cos;
+var log=Math.log;
+var Pi=Math.PI;
+
+
+
+var funcTab=[
+    function(x,y){ return(0);},
+    function(x,y){ return(x); },
+    function(x,y){ return(y); },
+    function(x,y){ return(sin(Pi*x)); },
+    function(x,y){ return(cos(Pi*x)); },
+    function(x,y){ return(sin(Pi*x)*sin(Pi*y)); },
+    function(x,y){ return(cos(Pi*x)*cos(Pi*y)); },
+    function(x,y){ return(cos(Pi*x)*sin(Pi*y)); },
+    function(x,y){ return(cos(Pi*x)*y); },
+    function(x,y){ return(sin(Pi*x)*y); },
+    function(x,y){ return(cos(Pi*(x*x+y*y))); },
+    function(x,y){ return(sin(Pi*(x*x+y*y))); }, 
+    function(x,y){ return(sin(Pi*(x*x+y*y))); },
+    function(x,y){ return(log(x*20+21));  }
+];
+
+var fx=funcTab[1];
+var fz=funcTab[2];
+var fy=funcTab[5];
+
+
+var xmin=-1;
+var xmax= 1;
+var ymin=-1;
+var ymax= 1;
+
+var ALPHA =(Pi/6);
+var BETA = (Pi/4);
+var rotstep = (Pi/12);
+
+var scalex= 5;
+var scaley= 5;
+var scalez= 5;
+// var sc_coefficient= (4.0/5.0);
+
+
+var perspective= function(eyex, eyez, screen_z, x,z)
+{
+    return( eyex+(screen_z-eyez)*(x-eyex)/(z-eyez) );
+}
+
+var xrotate= function (x,y, angle)
+{
+    return(x*cos(angle)-y*sin(angle));
+}
+
+var yrotate= function (x,y, angle)
+{
+    return(y*cos(angle)+x*sin(angle));
+}
+
+var rpixel=0.005;
+var rminx=-rpixel*ctx.canvas.width/2;
+var rmaxx= rpixel*ctx.canvas.width/2;
+var rminy=-rpixel*ctx.canvas.height/2;
+var rmaxy= rpixel*ctx.canvas.height/2;
+
+
+var rx= function (ctx, x )
+{
+    return (x-rminx)/(rmaxx-rminx)*(ctx.canvas.width);
+}
+
+var ry= function (ctx, y )
+{
+    return ctx.canvas.height-(y-rminy)/(rmaxy-rminy)*(ctx.canvas.height);
+}
+
+var rputpixel= function(ctx, x,y)
+{
+    if(rminx<=x && x<rmaxx && rminy<=y && y<rmaxy) ctx.fillRect(rx(ctx,x),ry(ctx,y),1,1);
+}
+
+var Xd= 0;
+var Yd= 1;
+var Zd= 2;
+
+var leftEye=[-3.5,0,-50];
+var rightEye=[3.5 ,0,-50];
+
+var screen_z= 0.0;
+
+var rightColor="rgb(0,0,255)";
+var leftColor ="rgb(127,0,0)";
+
+
+var draw=function(ctx, colorString, eye)
+{ 
+    var x,y;
+    var ffx, ffy, ffz, fffx, fffy, fffz;
+    var stepX=0.02;
+    var stepY=0.02;
+    ctx.fillStyle =colorString;
+
+    for(x=xmin; x<xmax; x+=stepX)
+    {
+	for(y=ymin; y<ymax; y+=stepY)
+	{
+            fffx=fx(x,y); fffy=fy(x,y); ffz= fz(x,y);
+	    
+	    /* horizontal rotation by ALPHA */
+            ffx= xrotate(fffx, ffz, ALPHA);
+            fffz= yrotate(fffx, ffz, ALPHA);
+
+	    /* vertical  rotation by BETA */
+            ffz=  xrotate(fffz, fffy, BETA);
+            ffy=  yrotate(fffz, fffy, BETA);
+	    
+            rputpixel( ctx,
+		       perspective( eye[Xd],eye[Zd], screen_z, ffx, ffz),
+                       perspective( eye[Yd],eye[Zd], screen_z, ffy, ffz)
+                     );
+	}
+    }
+}
+
+var clearScreen= function(ctx, bgColorString) {
+    ctx.globalCompositeOperation="source-over";
+    ctx.fillStyle=bgColorString;
+    ctx.fillRect(0,0, ctx.canvas.width, ctx.canvas.height );
+}
+
+
+var redraw= function() {
+    clearScreen(ctx, "rgb(0,0,0)");
+    draw(ctx, leftColor, leftEye );
+    clearScreen(ctx2, "rgb(0,0,0)");
+    draw(ctx2, rightColor, rightEye );
+    ctx.globalCompositeOperation="lighter";
+    ctx.drawImage(ctx2.canvas, 0,0);
+}
+
+redraw(); // test
+
+//////////////////////////////////////////////
+
+
+/*
+
+var red= function(x,y) {
+    return Math.sin(x)*Math.sin(y);
+}
+
+
+var green= function(x,y) {
+    return Math.sin(x)*Math.sin(y);
+}
+
+var blue= function(x,y) {
+    return -Math.sin(x)*Math.sin(y);
+}
+
+var redRange=[-1,1];
+var greenRange=[-1,1];
+var blueRange=[-1,1];
+
+var x,y, rx, ry, zRed, zGreen, zBlue;
+var xRange=[-2*Math.PI,2*Math.PI];
+var yRange=[-2*Math.PI,2*Math.PI];
+var xStep= (xRange[1]-xRange[0])/ctx.canvas.width;
+var yStep= (yRange[1]-yRange[0])/ctx.canvas.height;
+for( x=0; x<ctx.canvas.width; x++)
+    for( y=0; y<ctx.canvas.height; y++) {
+	rx= xRange[0]+x*xStep;
+	ry= yRange[0]+y*yStep;
+	zRed= Math.floor(255*(red(rx,ry)-redRange[0])/(redRange[1]-redRange[0]));
+	zGreen= Math.floor(255*(green(rx,ry)-greenRange[0])/(greenRange[1]-greenRange[0]));
+	zBlue= Math.floor(255*(blue(rx,ry)-blueRange[0])/(blueRange[1]-blueRange[0]));
+	ctx.fillStyle = "rgb("+zRed+","+zGreen+","+zBlue+")";
+
+	ctx.fillRect(x,y,1,1);
+    }
+console.log(ctx);
+*/
