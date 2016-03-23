36df823e666ac0f42168a57e90952b6d51dcaa9b
Marcin Kik <mki1967@gmail.com>
2016-03-23 08:55:15 +0100

    poprawki ...

--- webgl-points/points.js
+++ webgl-points/points.js
@@ -129,10 +129,11 @@ var redraw = function() {
     gl.vertexAttribPointer(glObjects.aVertexPositionLocation, 2 /* 2 floats per vertex */, gl.FLOAT, false, 0 /* stride */, 0 /*offset */);
     gl.drawArrays(gl.POINTS, 0 /* offset */, data.NUMBER_OF_VERTICES);
 
+    /* draw object 3 */
     gl.bindBuffer(gl.ARRAY_BUFFER, glObjects.bufferId3 ); /* refer to the buffer */
     gl.enableVertexAttribArray(glObjects.aVertexPositionLocation);
     gl.vertexAttribPointer(glObjects.aVertexPositionLocation, 2 /* 2 floats per vertex */, gl.FLOAT, false, 0 /* stride */, 0 /*offset */);
-    gl.drawArrays(gl.TRIANGLE_FAN, 0 /* offset */, 4);// data.vertexPositions3.length/2-2 );
+    gl.drawArrays(gl.TRIANGLE_FAN, 0 /* offset */, 4);
 
 };
 
